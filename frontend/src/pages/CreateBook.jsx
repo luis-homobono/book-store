aaabba8305c4d768bb33cfa190bef596e24ad716
Luis Eloy Homobono Fuentes <eloy.homobono@gmail.com>
2023-12-04 21:44:44 -0600

    add refactor in home componet and add services for api bookstore and components

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import { useNavigate } from "react-router-dom";
 
 const CreateBook = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [publishYear, setPublishYear] = useState();
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleSaveBook = () => {
+        const data = {
+            title,
+            author,
+            publishYear,
+        };
+        setLoading(true);
+        axios
+            .post('http://127.0.0.1:5555/books', data)
+            .then(() => {
+                setLoading(false);
+                navigate('/');
+            })
+            .catch((error) => {
+                setLoading(false);
+                alert('An error happened. Please Check console');
+                console.log(error);
+            })
+    }
+
     return (
-        <div>Create Book</div>
+        <div className="p-4 ">
+            <BackButton />
+            <h1 className="text-3xl m-4">Create Book</h1>
+            {loading ? <Spinner /> : ''}
+            <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+                <div className="my-4">
+                    <label className="text-xl mr-4 text-gray-500">Title</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="border-2 border-gray-500 px-4 w-full"
+                    />
+                </div>
+                <div className="my-4">
+                    <label className="text-xl mr-4 text-gray-500">Author</label>
+                    <input
+                        type="text"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                        className="border-2 border-gray-500 px-4 w-full"
+                    />
+                </div>
+                <div className="my-4">
+                    <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+                    <input
+                        type="number"
+                        value={publishYear}
+                        onChange={(e) => setPublishYear(e.target.value)}
+                        className="border-2 border-gray-500 px-4 w-full"
+                    />
+                </div>
+                <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
+                    Save
+                </button>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
 
 const DeleteBook = () => {
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const handleDeleteBook = () => {
+        setLoading(true);
+        axios
+            .delete(`http://127.0.0.1:5555/books/${id}`)
+            .then(() => {
+                setLoading(false);
+                navigate('/');
+            })
+            .catch((error) => {
+                setLoading(false);
+                alert('An error happened. Please Check console');
+                console.log(error);
+            })
+    }
+
     return (
-        <div>Delete Book</div>
+        <div className="p-4 ">
+            <BackButton />
+            <h1 className="text-3xl m-4">Delete Book</h1>
+            {loading ? <Spinner /> : ''}
+            <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+                <h3>Are you sure you want to delete this book?</h3>
+                <button
+                    className="p-4 bg-red-600 text-white m-8 w-full"
+                    onClick={handleDeleteBook}
+                >
+                    Yes, Delete it
+                </button>
+            </div>
+        </div>
     )
 }
 
